b3d049f86321ff48aaaf5583b9f353e484b9abf5
Igor Ryazanov <123439439+Greypfrut13@users.noreply.github.com>
2025-10-11 16:32:22 +0800

    Created Player Input

---
+++ Assets/Scripts/Player/Input/PlayerInputHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player.Input
+{
+    public class PlayerInputHandler : MonoBehaviour
+    {
+        [SerializeField] private InputActionReference _moveActionReference;
+        [SerializeField] private InputActionReference _jumpActionReference;
+        [SerializeField] private InputActionReference _attackActionReference;
+
+        public float HorizontalMoveInput { get; private set; }
+        public bool JumpPressed { get; private set; }
+        public bool AttackPressed { get; private set; }
+        
+        private void OnEnable()
+        {
+            _moveActionReference.action.Enable();
+            _jumpActionReference.action.Enable();
+            _attackActionReference.action.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _moveActionReference.action.Disable();
+            _jumpActionReference.action.Disable();
+            _jumpActionReference.action.Disable();
+        }
+
+        private void Update()
+        {
+            HorizontalMoveInput = _moveActionReference.action.ReadValue<float>();
+            JumpPressed = _jumpActionReference.action.IsPressed();
+            AttackPressed = _attackActionReference.action.IsPressed();
+        }
+    }
+}
+
---
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Player.Input;
+using UnityEngine;
+
+namespace Player.Movement
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private PlayerInputHandler _playerInputHandler;
+        
+    }
+}
